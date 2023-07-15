ed4f96c083136b25c9c30ff29a48882fb683259c
Majesty0 <mkyeremateng8as@gmail.com>
2023-07-15 14:18:26 -0700

    alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints the alphabet in lower case'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while
+		(n <= 122);
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
