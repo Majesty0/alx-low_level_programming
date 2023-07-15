1a3a5825f4c3165a32e9eb38ba385e8fc0f2cf99
Majesty0 <mkyeremateng8as@gmail.com>
2023-07-15 16:57:24 -0700

    numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints single whole numbers in base ten'
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while
+	(n <= 9)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
