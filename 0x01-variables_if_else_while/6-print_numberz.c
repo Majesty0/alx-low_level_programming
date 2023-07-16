13410a9bf0c5ffe3f792a1dc21d87ee7c611644f
Majesty0 <mkyeremateng8as@gmail.com>
2023-07-15 17:25:42 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry poitn
+ * Description: 'prints single numbers in base ten'
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while
+	(n <= 9)
+	{
+		putchar(n);
+		++n;
+	}
+	putchar('\n');
+	return (0);
+}
