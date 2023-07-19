3262dd61e794ba59862488bf127c134ca8ece043
Majesty0 <mkyeremateng8as@gmail.com>
2023-07-18 17:04:43 -0700

    abslute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - computes absolute number of an imteger
+ *@c: The number to be computed
+ * Return: Always 0.
+ */
+int _abs(int c)
+
+{
+	if (c <0 )
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
