46772a13df858896e2f28855b69f06e8ca8e455a
Majesty0 <mkyeremateng8as@gmail.com>
2023-08-02 13:25:50 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * palind2 - obtains length of a string
+ * @a: string
+ * @l: integer length
+ * Return: On success 1 and 0 on error
+ * errno is set appropriately.
+ */
+int palind2(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length of string5
+ * Return: On success 1 and 0 on error
+ * errno is set appropriately.
+ */
+
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate 
+ * Return: On success 1 and 0 on error
+ * errno is set appropriately.
+ */
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = palind2(s, 0);
+	return (palind3(s, l));
+}
