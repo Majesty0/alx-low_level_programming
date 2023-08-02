2f1b5b3e295d0c291c1e7416f0e0b58468f68220
Majesty0 <mkyeremateng8as@gmail.com>
2023-08-02 13:29:48 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -31,7 +31,7 @@ int palind3(char *a, int l)
 }
 /**
  * is_palindrome - checks if a string is a palindrome
- * @s: string to evaluate 
+ * @s: string to evaluate
  * Return: On success 1 and 0 on error
  * errno is set appropriately.
  */
